976b65568d12d4cd2dd543d72bc201a552cc8c2e
mpantelimonov <pantelimonovmm@gmail.com>
2022-01-28 00:45:52 +0200

    Finished gurock tests for hw20

--- src/test/java/com/ex/ui/GurockCompanyLInks.java
+++ src/test/java/com/ex/ui/GurockCompanyLInks.java
@@ -1,17 +1,28 @@
 package com.ex.ui;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.ArrayList;
 
 public class GurockCompanyLInks extends BaseTest {
     String homeUrl = "https://www.gurock.com/testrail/";
+
+    @Test
+    public void legalPolicesTab() {
+        webDriver.get(homeUrl);
+        WebElement webElement = webDriver.findElement(By.xpath("//a[contains(., 'Legal')]"));
+        scrollToElement(webDriver, webElement);
+        webElement.click();
+
+        ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+        webDriver.switchTo().window(tabs.get(1));
+        findElement(webDriver, By.xpath("//a[@id='ui-id-3']")).click();
+
+        Assert.assertTrue(findElement(webDriver, By.xpath("//a[contains(.,'Data Storage Policy')]")).isDisplayed());
+    }
 
     @Test
     public void aboutLink() {
@@ -71,13 +82,12 @@ public class GurockCompanyLInks extends BaseTest {
     @Test
     public void legalLink() {
         String expectedLegalUrl = "https://www.ideracorp.com/legal/Gurock";
+
         webDriver.get(homeUrl);
         WebElement webElement = findElement(webDriver, By.xpath("//a[contains(text(),'Legal')]"));
-
         scrollToElement(webDriver, webElement);
         webElement.click();
-
-        ArrayList<String> tabs = new ArrayList<String> (webDriver.getWindowHandles());
+        ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
         webDriver.switchTo().window(tabs.get(1));
 
         String actualUrl = webDriver.getCurrentUrl();
@@ -87,12 +97,13 @@ public class GurockCompanyLInks extends BaseTest {
         webDriver.close();
         webDriver.switchTo().window(tabs.get(0));
         webDriver.manage().window().maximize();
-
-        WebElement subscribeBtn = waitElementClickable(webDriver, By.xpath("//div[@class='col-6 col-sm-6 col-md-4 col-lg-3 gk-footer-menu-item-first']/a"));
+        WebElement subscribeBtn = waitElementClickable(webDriver, By.xpath("//div[@class='col-6 col-sm-6 col-md-4 " +
+                "col-lg-3 gk-footer-menu-item-first']/a"));
         scrollToElement(webDriver, subscribeBtn);
         subscribeBtn.click();
 
+        actualUrl = webDriver.getCurrentUrl();
 
-
+        Assert.assertEquals(actualUrl, homeUrl + "newsletter/");
     }
 }
